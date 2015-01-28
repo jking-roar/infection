a3aa74e357ca803a6f1c3f3cdd4871ec94286996
Josh King <josh.king@pivotal-guest-213.la.pivotallabs.com>
2015-01-27 20:31:10 -0800

    got activity

---
+++ src/com/example/infect/Start.java
@@ -0,0 +1,13 @@
+package com.example.infect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Start extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+    }
+}
